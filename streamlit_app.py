1d48ac6c0e885451144bad5c5ba37c1c565ca147
SubhashreePraharaj <124771485+SubhashreePraharaj@users.noreply.github.com>
2023-02-13 17:27:57 +0530

    Update streamlit_app.py
    Pre populate the fruit list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,11 @@
 import streamlit
 streamlit.title('Subhashree')
 streamlit.header('My Profile')
-streamlit.text('I am a Data Engineer')
+streamlit.text('I am a "Happy-Go-Lucky" person')
 streamlit.header('🍌🥭 Stay Healthy 🥝🍇')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strwaberry'])
 streamlit.dataframe(my_fruit_list)
