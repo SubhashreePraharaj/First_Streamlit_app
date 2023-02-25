1d6904069414a9b366ba643d25c1832cc225e31e
SubhashreePraharaj <124771485+SubhashreePraharaj@users.noreply.github.com>
2023-02-26 01:29:01 +0530

    try and except

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,11 +12,16 @@ streamlit.dataframe(my_fruit_list)
 
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
-fruityvice_normalised = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalised)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_choice:
+    streamlit.error("please select a fruit to get information")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+    fruityvice_normalised = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalised)
+except URLError as e:
+  streamlit.error()
 import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
