ec4b8bb6e5093866b7d61959d1c697858257ff6a
SubhashreePraharaj <124771485+SubhashreePraharaj@users.noreply.github.com>
2023-02-25 21:01:09 +0530

    full code added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,17 @@
 import streamlit
-streamlit.header("Fruityvice Fruit Advice!")
+streamlit.title('Subhashree')
+streamlit.header('My Profile')
+streamlit.text('I am a "Happy-Go-Lucky" person')
+streamlit.header('🍌🥭 Stay Healthy 🥝🍇')
 import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+streamlit.dataframe(my_fruit_list)
+
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
