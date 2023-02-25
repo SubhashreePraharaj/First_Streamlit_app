4ae157843f95f9934e39f657e9f69073cec1547e
SubhashreePraharaj <124771485+SubhashreePraharaj@users.noreply.github.com>
2023-02-25 20:57:19 +0530

    added json()

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ import pandas
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
